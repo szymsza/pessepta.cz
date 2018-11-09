2f0b5eccc406b04d179fb7f9b16db2d6914b8ea2
Jakub Szymsza <jakub@szymsza.cz>
2018-11-10 00:53:00 +0100

    Login

--- src/views/login.php
+++ src/views/login.php
@@ -1,7 +1,23 @@
 <div id="page-login">
-	<h1>Vítejte v Pes se ptá</h1>
-	<p>Vítáme vás v naší seznam hře.<br>
-	 Náplní celé hry je uhodnout, který z výrazů byl vícekrát za danou dobu vyhledán.</p>
-	 <button>Singleplayer</button>
-	 <button>Multiplayer</button>
+	<h1>Vítejte v Pes se ptá!</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col m12">	
+				<p>Vítáme vás ve hře Pes se ptá!</p>
+	 			<p>Náplní celé hry je uhodnout, který z výrazů byl vícekrát za danou dobu vyhledán.</p>
+			</div>
+			<div class="col m6">
+	 			<button class="waves-effect waves-light btn-large red darken-1">
+	 				<img src="imgs/forever_alone.png">
+				 	Singleplayer
+			 	</button>
+			</div>
+			<div class="col m6">
+	 			<button class="waves-effect waves-light btn-large red darken-1">
+	 				<img src="imgs/like_a_boss.png">
+	 				Multiplayer
+	 			</button>
+			</div>
+		</div>
+	</div>
 </div>
