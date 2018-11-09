acdbd16944dfa445e8195dc2e7511e7a2c30423d
greplh <greplh@seznam.cz>
2018-11-10 00:09:00 +0100

    CSS

---
+++ src/views/oblasti.php
@@ -0,0 +1,18 @@
+<div id="oblasti">
+	<h1>Zvolte oblasti otázek:</h1>
+	<input type="checkbox">Osobnosti<br>
+	<input type="checkbox" class="spodní">Politici<br>
+	<input type="checkbox" class="spodní">Sportovci<br>
+	<input type="checkbox" class="spodní">Hudebníci<br>
+	<input type="checkbox" class="spodní">Herci<br>
+	<input type="checkbox" class="spodní">Spisovatelé<br>
+	<input type="checkbox">Novinky<br>
+	<input type="checkbox">Počasé<br>
+	<input type="checkbox">Recepty<br>
+	<input type="checkbox">Hudba<br>
+	<input type="checkbox">Mapy<br>
+	<input type="checkbox">Zoo<br>
+	<input type="checkbox">Demagog<br>
+
+
+</div>
---
+++ src/views/obtiznost.php
@@ -0,0 +1,5 @@
+<div id="obtiznost">
+	<h1>Zvolte obtížnost:</h1>
+	<button>Těžké</button>
+	<button class="lehke">Lehké</button>
+</div>
