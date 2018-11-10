40efcb02c249fb8566980655ad5fbd3deec3f290
Jakub Szymsza <jakub@szymsza.cz>
2018-11-10 08:09:33 +0100

    Voice fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,25 @@
 import Page from "./Page.js"
 import Hund from "./Hund.js"
 
+function greet() {
+	Hund.say([
+		"haf haf!",
+		"ahoj seznamáku!",
+		"vítej ve hře Pes se ptá, neboli já, Krasty, se ptám",
+		"chceš si hrát sám, nebo s přáteli?"
+	]);
+}
+
 $(document).ready(function() {
 	var page = new Page()
 	page.move("login")
 
 	setTimeout(function() {
-		Hund.say([
-			"haf haf!",
-			"ahoj seznamáku!",
-			"vítej ve hře Pes se ptá, neboli já, Krasty, se ptám",
-			"chceš si hrát sám, nebo s přáteli?"
-		]);
+		// sound won't play until document has focus
+		if (document.hasFocus())
+			greet()
+		else
+			$(window).one("focus", greet);
 	}, 500);
 
 	$(".hund img").click(function() {
