e4c45d41bde2b1c37b983da7d5547093d1589a7a
greplh <greplh@seznam.cz>
2018-11-10 01:46:47 +0100

    Hund

---
+++ src/views/questions.php
@@ -0,0 +1,9 @@
+<div id="page-questions">
+	<h1> Jaká z položek níže je více vyhledávaná?</h1><br>
+	<h2 class="score" id="score">Skóre: </h2><br>
+	<br>
+	<br>
+	<br>
+	<button></button><button></button>
+	
+</div>
