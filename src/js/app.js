31c1097777d3696bcf5a04dc3b3f42c7a6e10a30
Jakub Szymsza <jakub@szymsza.cz>
2018-11-10 04:33:32 +0100

    Fix

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,13 @@ import Hund from "./Hund.js"
 
 $(document).ready(function() {
 	var page = new Page()
-	page.move("categories")
+	page.move("login")
 
-	/*
 	Hund.say([
 		"ahoj píčo!",
 		"vítej ve hře Pes se ptá, teda já se ptám, haha",
 		"vyber, jestli jsi osamělá kunda, nebo máš nějaký kámoše, se kterýma chceš hrát"
-	]);*/
+	]);
 
 	$(".hund img").click(function() {
 		Hund.say("haf haf")
