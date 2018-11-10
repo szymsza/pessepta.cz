913384a2bf930bedb2496f5d7138df76b9e458c8
Jakub Szymsza <jakub@szymsza.cz>
2018-11-10 06:25:00 +0100

    Nicer categories

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
 	var page = new Page()
 	page.move("login")
 
-	setTimeout(function() {
+	/*setTimeout(function() {
 		Hund.say([
 			"ahoj píčo!",
 			"vítej ve hře Pes se ptá, teda já se ptám, haha",
 			"vyber, jestli jsi osamělá kunda, nebo máš nějaký kámoše, se kterýma chceš hrát"
 		]);
-	}, 500);
+	}, 500);*/
 
 	$(".hund img").click(function() {
 		Hund.say("haf haf")
--- src/views/categories.php
+++ src/views/categories.php
@@ -2,25 +2,24 @@
 	<h1>Zvolte oblasti otázek:</h1>
 	<div class="container">
 		<div class="row">
+			<div class="col m5">
+			</div>
 			<div class="col m6">
 				<label>
 	 				<input type="checkbox" class="filled-in" checked data-value="celebrities">
 	 				<span>Celebrity</span>
 	 			</label>
-			</div>
-	 		<div class="col m6">
+
 	 			<label>
 	 				<input type="checkbox" class="filled-in" data-value="hovno">
 	 				<span>Recepty</span>
 	 			</label>
-	 		</div>
-	 		<div class="col m6">
+
 	 			<label>
 	 				<input type="checkbox" class="filled-in" data-value="hovno">
 	 				<span>Mapy</span>
 	 			</label>
-	 		</div>
-	 		<div class="col m6">
+
 	 			<label>
 	 				<input type="checkbox" class="filled-in" data-value="hovno">
 	 				<span>Ostatní</span>
