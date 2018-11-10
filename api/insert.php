20b90765bd6ce7fb909b34a02a06728254f73ae1
Jakub Szymsza <jakub@szymsza.cz>
2018-11-10 05:49:09 +0100

    API

--- api/insert.php
+++ api/insert.php
@@ -1,9 +1,58 @@
 <?php
 
 header('Content-Type: application/json');
+
+$data = $_GET;
+$insert = [
+	"text" => mb_convert_case(trim($data["dotaz"]), MB_CASE_TITLE, "UTF-8"),
+	"year" => (int)$data["rok"],
+	"month" => (int)$data["mesic"],
+	"day" => (int)$data["den"],
+	"category" => trim($data["kategorie"]),
+	"image" => trim($data["image"])
+];
+
+switch ($insert["category"]) {
+	case 'celebrity':
+		$insert["category"] = "celebrities";
+		break;
+
+	case 'mapy':
+		$insert["category"] = "maps";
+		break;
+
+	case 'recepty':
+		$insert["category"] = "recipe";
+		break;
+
+	case 'porno':
+		$insert["category"] = "porn";
+		break;
+	
+	default:
+		$insert["category"] = "other";
+		break;
+}
+
+// Query already exists
+if ($db->query("SELECT * FROM `records` WHERE `text` = '".$insert["text"]."'")->fetchColumn())
+	die(json_encode([
+		"ok" => false,
+		"message" => "already exists"
+	]));
+
+try {
+$db->prepare("INSERT INTO `records` (`text`, `year`, `month`, `day`, `category`, `image`) VALUES (:text, :year, :month, :day, :category, :image)")->execute($insert);
+    } catch( PDOEXception $e ) {
+
+
+           echo $e->getMessage(); // display error
+           exit();
+
+
+    }
+
+
 echo json_encode([
-	"ok" => true,
-	"post" => $_POST,
-	"get" => $_GET,
-	"request" => $_REQUEST
+	"ok" => true
 ]);
--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,13 @@ $(document).ready(function() {
 	var page = new Page()
 	page.move("login")
 
-	Hund.say([
-		"ahoj píčo!",
-		"vítej ve hře Pes se ptá, teda já se ptám, haha",
-		"vyber, jestli jsi osamělá kunda, nebo máš nějaký kámoše, se kterýma chceš hrát"
-	]);
+	setTimeout(function() {
+		Hund.say([
+			"ahoj píčo!",
+			"vítej ve hře Pes se ptá, teda já se ptám, haha",
+			"vyber, jestli jsi osamělá kunda, nebo máš nějaký kámoše, se kterýma chceš hrát"
+		]);
+	}, 500);
 
 	$(".hund img").click(function() {
 		Hund.say("haf haf")
