3d0a9462fd177b06d90d8fe65c46206818640930
Jakub Szymsza <jakub@szymsza.cz>
2018-11-10 01:04:01 +0100

    Hund

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,7 @@ import Page from "./Page.js"
 
 var page = new Page()
 page.move("login")
+
+setInterval(function() {
+	$(".hund img").attr("src", $(".hund img").attr("src") == "imgs/seznam_hund.gif" ? "imgs/seznam_hund.png" : "imgs/seznam_hund.gif")
+}, 2000);
