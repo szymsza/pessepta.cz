d296a4801f565b644a721d0591f8994c71f4e082
Jakub Szymsza <jakub@szymsza.cz>
2018-11-10 01:14:32 +0100

    Dist

--- src/js/Page.js
+++ src/js/Page.js
@@ -1,7 +1,16 @@
 class Page {
+	constructor() {
+		console.log("scam");
+		this.afterMove = {
+			scam: "scam"
+		};
+	}
+
 	move(pageName) {
 		$("#pages>div").hide()
 		$("#pages>#page-"+pageName).show()
+
+		console.log(this.afterMove)
 	}
 }
 
